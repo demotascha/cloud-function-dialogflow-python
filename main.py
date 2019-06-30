2d81d904c3f9562326081f4a26ac054136c7f898
Demo <demotascha@gmail.com>
2019-06-30 14:33:56 +0800

    feat: 使用 CarouselTemplate

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
     ImageSendMessage,
     LocationMessage,
+    CarouselTemplate, CarouselColumn, URIAction,
     TemplateSendMessage, ButtonsTemplate, URITemplateAction,
 )
 
@@ -129,44 +130,55 @@ def handle_location_message(event):
             KeyError
     if len(above4) < 0:
         print('no 4 start resturant found')
-    # 3. 隨機選擇一間餐廳
+    # 3. 隨機選擇一間餐廳
         restaurant = random.choice(top20_restaurants)
-    restaurant = top20_restaurants[random.choice(above4)]
-    # 4. 檢查餐廳有沒有照片，有的話會顯示
-    if restaurant.get("photos") is None:
-        thumbnail_image_url = None
-    else:
-        # 根據文件，最多只會有一張照片
-        photo_reference = restaurant["photos"][0]["photo_reference"]
-        thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
-    # 5. 組裝餐廳詳細資訊
-    rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
-    address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
-    details = "評分：{}\n地址：{}".format(rating, address)
 
-    # 6. 取得餐廳的 Google map 網址
-    map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
-        lat=restaurant["geometry"]["location"]["lat"],
-        long=restaurant["geometry"]["location"]["lng"],
-        place_id=restaurant["place_id"]
-    )
-    # 使用 Google API End =========
+    # 隨機選擇三間
+    cc=[]
+    count = 0
+    while count < 3:
+        restaurant = top20_restaurants[random.choice(above4)]
+        # 4. 檢查餐廳有沒有照片，有的話會顯示
+        if restaurant.get("photos") is None:
+            thumbnail_image_url = None
+        else:
+            # 根據文件，最多只會有一張照片
+            photo_reference = restaurant["photos"][0]["photo_reference"]
+            thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
+        # 5. 組裝餐廳詳細資訊
+        rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
+        address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+        details = "評分：{}\n地址：{}".format(rating, address)
 
-    # 回覆使用 Buttons Template
-    buttons_template_message = TemplateSendMessage(
-    alt_text=restaurant["name"],
-    template=ButtonsTemplate(
+        # 6. 取得餐廳的 Google map 網址
+        map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+            lat=restaurant["geometry"]["location"]["lat"],
+            long=restaurant["geometry"]["location"]["lng"],
+            place_id=restaurant["place_id"]
+        )
+        # 建立 CarouselColumn obj
+        c = CarouselColumn(
             thumbnail_image_url=thumbnail_image_url,
             title=restaurant["name"],
             text=details,
             actions=[
-                URITemplateAction(
+                URIAction(
                     label='查看 Google 地圖',
                     uri=map_url
-                ),
+                )
             ]
+        )
+        # 加入 list
+        cc.append(c)
+        count += 1
+
+    # 回覆使用 Carousel Template
+    carousel_template_message = TemplateSendMessage(
+        alt_text='選出三間推薦給你',
+        template=CarouselTemplate(
+            columns=cc
         )
     )
     line_bot_api.reply_message(
         event.reply_token,
-        buttons_template_message)
+        carousel_template_message)
